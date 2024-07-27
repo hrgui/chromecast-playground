cca5f4eebcd36591eaeeb0f0384c1d1ec07a1c94
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-07-27 16:48:42 -0700

    feat: demonstrate reliability of timing

--- src/setupShutdownListener.ts
+++ src/setupShutdownListener.ts
@@ -1,16 +1,23 @@
 import { CastReceiverContext } from "chromecast-caf-receiver/cast.framework";
 
-function makeBeaconRequest({ id, url = "/api/beacon" }: { id: number; url?: string }) {
-  const data = new Blob([JSON.stringify({ message: "Hello, Server! " + id + Date.now() })], {
-    type: "application/json",
-  });
-
-  // Send data using navigator.sendBeacon
+function makeBeaconRequest({
+  id,
+  url = "/api/beacon",
+  message = "Hello Server!",
+}: {
+  id: number;
+  url?: string;
+  message?: string;
+}) {
+  const data = JSON.stringify({ timestamp: Date.now(), id, message });
   navigator.sendBeacon(url, data);
 }
 
 export function setupShutdownListener(context: CastReceiverContext) {
+  makeBeaconRequest({ id: 0, message: "I am starting up." });
   context.addEventListener(cast.framework.system.EventType.SHUTDOWN, () => {
-    makeBeaconRequest({ id: 1 });
+    for (let i = 1; i <= 10; i++) {
+      makeBeaconRequest({ id: i, message: "I am shutting down." });
+    }
   });
 }
