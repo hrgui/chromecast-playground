728132ec74b4187008c154154bfe389309b57ee3
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-07-27 17:32:51 -0700

    feat: make beacon requests simpler

--- src/setupShutdownListener.ts
+++ src/setupShutdownListener.ts
@@ -3,21 +3,21 @@ import { CastReceiverContext } from "chromecast-caf-receiver/cast.framework";
 function makeBeaconRequest({
   id,
   url = "/api/beacon",
-  message = "Hello Server!",
+  type = "STARTUP",
 }: {
   id: number;
   url?: string;
-  message?: string;
+  type?: string;
 }) {
-  const data = JSON.stringify({ timestamp: Date.now(), id, message });
+  const data = JSON.stringify({ timestamp: Date.now(), id, type });
   navigator.sendBeacon(url, data);
 }
 
 export function setupShutdownListener(context: CastReceiverContext) {
-  makeBeaconRequest({ id: 0, message: "I am starting up." });
+  makeBeaconRequest({ id: 0, type: "STARTUP" });
   context.addEventListener(cast.framework.system.EventType.SHUTDOWN, () => {
     for (let i = 1; i <= 10; i++) {
-      makeBeaconRequest({ id: i, message: "I am shutting down." });
+      makeBeaconRequest({ id: i, type: "SHUTDOWN" });
     }
   });
 }
